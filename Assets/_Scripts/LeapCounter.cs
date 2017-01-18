72ff95a8995e53e82af37b17dd5b2521489eec94
hanafusa <zwrnrnrn@gmail.com>
2017-01-18 23:23:52 +0900

    飛距離計測処理を中心にコードを整理した
    新たに追加したLeapCounterクラスで飛距離を計算する。計測開始地点を超えたかどうか、着地したかどうかを判定するのもこのクラス。
    スコア(飛距離)の表示処理はScoreBoardクラスで行う。

---
+++ Assets/_Scripts/LeapCounter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 飛距離(leap)を数える。Recording Start Lineを超えた後に計測開始。
+/// プレイヤーは負のz軸方向に進んでいくので、Recording Start Lineのz座標からプレイヤーのz座標
+/// を引いた差を飛距離とする
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class LeapCounter : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject recordingStartLine;
+
+	private bool isPassed; // プレイヤーがRecordingStartLineを超えたかどうか
+	private Rigidbody rig;
+
+	public float leap // 飛距離
+	{
+		get;
+		private set;
+	}
+
+	// プレイヤーが着地したかどうか
+	public bool isLanded
+	{
+		get;
+		private set;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		isPassed = false;
+		isLanded = false;
+		leap = -1;
+		rig = GetComponent<Rigidbody>();
+		rig.constraints = RigidbodyConstraints.FreezeRotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// 飛距離を計算
+		leap = recordingStartLine.transform.position.z - transform.position.z;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		// Recording Start Lineを超えたら計測開始
+		if (col.gameObject == recordingStartLine)
+		{
+			isPassed = true;
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		// Record Start Lineを超えた後に着地したらPlayerが動かないようにする
+		if (isPassed)
+		{
+			isLanded = true;
+			rig.isKinematic = true;
+		}
+	}
+}
---
+++ Assets/_Scripts/ScoreBoard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Canvas))]
+public class ScoreBoard : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+	[SerializeField]
+	private Text scoreText;
+
+	private LeapCounter leapCounter;
+	private Canvas scoreBoardCanvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+		leapCounter = player.GetComponent<LeapCounter>();
+		scoreBoardCanvas = GetComponent<Canvas>();
+		scoreBoardCanvas.enabled = false; // スコアボードを隠しておく
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// 着地後ならスコアボードを表示する
+		if (leapCounter.isLanded)
+		{
+			scoreText.text = string.Format("{0:F1}m", leapCounter.leap);
+			scoreBoardCanvas.enabled = true;
+		}
+	}
+}
