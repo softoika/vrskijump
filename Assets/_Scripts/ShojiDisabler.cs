426fa430feb84b39d2812816dca487b01fd7f6dc
hanafusa <zwrnrnrn@gmail.com>
2017-01-29 23:59:57 +0900

    スコア表示を塞ぐことがあるので着地時に障子を全て消す→ShojiDisabler
    スキージャンプ台も全長を長く、傾斜鋭くなるよう調整した。

---
+++ Assets/_Scripts/ShojiDisabler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShojiDisabler : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject shojiFolder;
+
+	[SerializeField]
+	private PlayerState playerState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerState.isLanding)
+		{
+			shojiFolder.SetActive(false);
+		}
+	}
+}
