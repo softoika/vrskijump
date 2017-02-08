2e3bbecc5e7e25496ef298863b612aca8c80098f
hanafusa <zwgntz@gmail.com>
2017-02-08 17:50:37 +0900

    追加エリアの製作開始＋Global Fogの設定

---
+++ Assets/_VR_Ski_Jump/Scripts/GlobalFogSetting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Starndard AssetsのGlobal Fogの設定
+/// </summary>
+public class GlobalFogSetting : MonoBehaviour {
+
+	[SerializeField]
+	private Color fogColor;
+	void Start () {
+		// フォグカラーの設定
+		RenderSettings.fogColor = fogColor;
+	}
+}
